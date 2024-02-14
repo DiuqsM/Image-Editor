08478fdbec1c030d2634815e417c7f5e1ec74d24
Rihui Lu <rihuilu1108@gmail.com>
2024-02-14 17:14:39 -0500

    Changed to image editor

---
+++ dialog.js
@@ -0,0 +1,26 @@
+const {dialog, app, BrowserWindow} = require('electron');
+const fs = require('fs');
+const path = require('path');
+
+module.exports = {
+  showSaveDialog,
+  showOpenDialog
+}
+
+function showSaveDialog(BrowserWindow){
+    //not implemeted yet
+  }
+
+
+function showOpenDialog(BrowserWindow){
+    dialog.showOpenDialog(BrowserWindow,{
+      defaultPath: app.getPath("downloads"),
+      filters: [
+        { name: "Image", extensions: ["jpg", "png", "jpeg"]}
+      ]
+    }, (filepaths) => {
+      if(filepaths) {
+        console.log(filepaths, fs.readFileSync(filepaths[0], 'utf8'));
+      }
+    })
+  }
--- main.js
+++ main.js
@@ -1,40 +1,15 @@
+const {app, BrowserWindow, document} = require('electron');
 const path = require('path');
-const {app, BrowserWindow, globalShortcut} = require('electron');
-
-function createWindow() {
-  const mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
-    webPreferences: { nodeIntegration: true }
-  });
-
-    mainWindow.loadFile(path.join(__dirname, 'index.html'));
-}
-
-function coordinate(event) {
-  let x = event.clientX;
-  let y = event.clientY;
-  document.getElementById("X").value = x;
-  document.getElementById("Y").value = y;
-}
-
-// if pressing ctrl + alt + shift
-// screen_shot = true
-// if press esc , screen_shot = false
-// on click, save coordinate to data 
-// on second click save coordinate to data
-// screen_shot = false 
-// take screenshot of the data 
-// calculate the area and take the screenshot of that specific area 
-// 1 (download the image and post it to google images in the browser app)
-// 2 (open a google image search page and post the image to the search bar)
-// redirect the user to the google images page
+const { showOpenDialog, showSaveDialog} = require('./dialog');
 
 
-//need two frameworks 
+let mainWindow;
+
 app.whenReady().then(() => {
-  // Register a 'CommandOrControl+X' shortcut listener.
+  mainWindow = new BrowserWindow();
+  mainWindow.loadFile(path.join(__dirname, 'index.html'));
 
-  createWindow();
+  let button = document.getElementById("input_file_btn");
+  button.addEventListener("click", showOpenDialog(mainWindow));
 
 }) ;
